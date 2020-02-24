04567cc7191f035364acfe3c8aa857186ce2c7fd
Samadi van Koten <samadi@vktec.org.uk>
2020-02-24 00:12:34 +0000

    Switch to using logger options for filtering

--- src/main/java/vktec/redlog/RedEventLogger.java
+++ src/main/java/vktec/redlog/RedEventLogger.java
@@ -15,6 +15,7 @@ import carpet.utils.Messenger;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.WeakHashMap;
 import net.minecraft.block.BlockState;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerWorld;
@@ -42,8 +43,6 @@ public class RedEventLogger {
 		indentLevel = 0;
 	}
 
-	private static CarpetContext scriptContext = new CarpetContext(CarpetServer.scriptServer.globalHost, null, new BlockPos(0, 0, 0));
-
 	private static String getIndentString() {
 		if (indentLevel == 0) return "";
 		if (indentLevel == indentBuffer.length()) return indentString;
@@ -58,7 +57,9 @@ public class RedEventLogger {
 	}
 
 	private static final String TEMPLATE = "[%d] %s (%d, %d, %d) %s %s";
-	private static final Object2ObjectOpenHashMap<String,RedEventLogger> cache = new Object2ObjectOpenHashMap(4);
+	private static final Map<String,RedEventLogger> loggerCache = new Object2ObjectOpenHashMap<>(5);
+	private static final Map<String, CarpetExpression> filterCache = new WeakHashMap<>();
+	private static final CarpetContext scriptContext = new CarpetContext(CarpetServer.scriptServer.globalHost, null, new BlockPos(0, 0, 0));
 
 	private static final Map<String,String> name2kind = Map.of(
 		"block36", "B36",
@@ -69,31 +70,32 @@ public class RedEventLogger {
 	);
 
 	public static RedEventLogger get(String logName) {
-		return cache.computeIfAbsent(logName, (name) -> new RedEventLogger(name));
+		return loggerCache.computeIfAbsent(logName, (name) -> new RedEventLogger(name));
 	}
 
 	private final Logger logger;
 	private final String kind;
-	private CarpetExpression filter;
 
 	private RedEventLogger(String logName) {
 		this.logger = LoggerRegistry.getLogger(logName);
 		this.kind = name2kind.get(logName);
-		this.setFilter(null, RedlogExtension.getFilter(logName));
 	}
 
-	public void setFilter(ServerCommandSource commandSource, String filter) {
-		if (filter == null || filter == "") {
-			this.filter = null;
-		} else {
-			this.scriptContext.s = commandSource;
-			this.filter = new CarpetExpression(RedEventLogger.scriptContext.host.main, filter, null, null);
+	private static CarpetExpression computeOrGetFilter(String filterString) {
+		CarpetExpression filter = RedEventLogger.filterCache.get(filterString);
+		if (filter == null) {
+			filter = new CarpetExpression(RedEventLogger.scriptContext.host.main, filterString, null, null);
+			RedEventLogger.filterCache.put(filterString, filter);
 		}
+		return filter;
 	}
 
-	private boolean runFilter(RedEvent ev) {
-		if (this.filter == null) return true;
+	private boolean runFilter(String filterString, RedEvent ev) {
+		if (filterString == null || filterString == "") return true;
 		if (CarpetServer.scriptServer.stopAll) return false;
+
+		final CarpetExpression filter = RedEventLogger.computeOrGetFilter(filterString);
+
 		try {
 			// TODO: add event-specific extra info variables here
 			RedEventLogger.scriptContext
@@ -103,8 +105,8 @@ public class RedEventLogger {
 				.with("z", (c, t) -> new NumericValue(ev.pos.getZ()).bindTo("z"))
 				.with("info", (c, t) -> new StringValue(ev.getInfoString()));
 			// XXX: gnembon/fabric-carpet#157
-			//return this.filter.getExpr().eval(RedEventLogger.scriptContext).getBoolean();
-			return RedlogHaxxor.evalCarpetExpression(this.filter, RedEventLogger.scriptContext).getBoolean();
+			//return filter.getExpr().eval(RedEventLogger.scriptContext).getBoolean();
+			return RedlogHaxxor.evalCarpetExpression(filter, RedEventLogger.scriptContext).getBoolean();
 		} catch (ExpressionException e) {
 			// TODO: Probably want better reporting than this
 			System.out.println(e);
@@ -129,23 +131,20 @@ public class RedEventLogger {
 	public void log(RedEvent ev) {
 		if (!this.enabled()) return;
 
-		if (this.filter != null) {
-			if (!this.runFilter(ev)) return;
-		}
+		this.logger.log((filterString) -> {
+			if (!this.runFilter(filterString, ev)) return null;
 
-		String info = ev.getInfoString();
-		if (info != null) info += " ";
-		final String finalInfo = info; // 'cause Java complains at me otherwise
+			String info = ev.getInfoString();
+			if (info != null) info += " ";
 
-		final String blockName = Registry.BLOCK.getId(ev.block.getBlock()).getPath();
+			final String blockName = Registry.BLOCK.getId(ev.block.getBlock()).getPath();
 
-		this.logger.log(() -> {
 			return new BaseText[]{Messenger.c(
 				String.format("g [%d] ", ev.world.getTime()),
 				"w " + RedEventLogger.getIndentString(),
 				"y " + this.kind + " ",
 				Messenger.tp("c", ev.pos),
-				"p  " + finalInfo,
+				"p  " + info,
 				"g " + blockName
 			)};
 		});
